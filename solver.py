bf2d3f013fed39d0afd332efcc21b5c68aba908e
dinodan20 <dobichie@umass.edu>
2020-02-08 13:36:18 -0500

    Update solver.py

--- solver.py
+++ solver.py
@@ -3,12 +3,26 @@
 # date:     2/5/2020
 # class:    COMPSCI 383 Artificial Intelligence
 # *************************************************
-import puzz.py
+import puzz
+import queue
 
-class search(EightPuzzleBoard):
+class search(puzz.EightPuzzleBoard):
 
     def breadth_search(self, solution, i = 0):
         #
+        frontier = queue.Queue()
+        frontier.put(self)
+        explored = []
+
+        while not frontier.empty():
+            node = frontier.get()
+            explored.append(node)
+            for n in node.successors():
+                if (n not in frontier) and (n not in explored):
+                    if n == solution:
+                        return n
+                    else:
+                        frontier.put(n)
         # if board = goal state:
         # return number of steps
         # x = success up()
@@ -17,7 +31,7 @@ class search(EightPuzzleBoard):
         # breadth_search(y)
         # ..................
         #
-        return str
+        return "fail"
 
     def uniform_cost_search(self, solution, i = 0):
         return 0
@@ -26,12 +40,19 @@ class search(EightPuzzleBoard):
         return 0
 
     def a_star_search(self, solution, i = 0):
-
+        return 0
 
 
 
 if __name__ == '__main__':
-    board = EightPuzzleBoard('12345678')
-    solution = EightPuzzleBoard('12345678')
-    board.breadth_search(solution)
-    board.uniform_cost_search(solution)
+
+    #board = EightPuzzleBoard('12345678')
+    board = puzz.EightPuzzleBoard('1234')
+    solution = puzz.EightPuzzleBoard('')
+    problm = search()
+    problm.breadth_search('123470568', '12')
+
+   # solution = EightPuzzleBoard('12345678')
+   # board.breadth_search(solution)
+   # board.uniform_cost_search(solution)
+
