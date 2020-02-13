e16f1ac3c61ae0471cc04ef4f670c195cc631732
dinodan20 <dobichie@umass.edu>
2020-02-12 19:35:10 -0500

    Update solver.py

--- solver.py
+++ solver.py
@@ -5,60 +5,67 @@
 # *************************************************
 import puzz
 import queue
+import copy
+from time import time
 
 class EightPuzzleBoard_Solver(puzz.EightPuzzleBoard):
 
     def breadth_search(self, solution):
-        #
+        if solution is None: #initial condition
+            return "invalid goal state"
+        # declare variables/objects/lists
+        to = time
         frontier = queue.Queue()
+        self.add_path(self, "start")
         frontier.put(self)
         explored = []
-        count = 0
+        frontCount = 1
+        # start looping through the breadth
         while not frontier.empty():
-            node = frontier.get()
-            explored.append(node)
-            for m, n in node.successors().items():
-                # ******** this section of code checks "n not in frontier" ********
-                boo = False
-                temp = frontier
-                while not temp.empty():
-                    node = temp.get()
-                    if node is n:
-                        boo = True
-                # ********************
-                if n and (n not in explored) and (not boo) :
-                    count += 1
-                    if n == solution:
-                        return count
+            node = frontier.get()       # pop from the frontier
+            explored.append(node)       # add the board to the explored list
+            for m, n in node.successors().items():      # obtain all possible moves, m = "left", "right", etc. n = puuzzle board
+                if n and (n not in explored) and (not self.in_queue(frontier, n)) :
+                    n._path = copy.deepcopy(node._path) #deep copy the traveled course
+                    n.add_path(n, m)
+                    if str(n) == str(solution):         # check if destination reached
+                        for k, v in n._path.items():    # loop to print the route
+                            print("{}\t{}".format(v,k))
+                        return "success:\npath cost: {}\nfrontier: {}\nexplored: {}".format(len(n._path)-1, frontCount, len(explored))
                     else:
+                        frontCount += 1
                         frontier.put(n)
-        # ..................
-        #
-        return "fail"
+        return "fail:\npath cost: {}\nfrontier: {}\nexplored: {}".format(len(n._path)-1, frontCount, len(explored))
 
 
 
     def uniform_cost_search(self, solution, i = 0):
+        if solution is None:  # initial condition
+            return "invalid goal state"
+            # declare variables/objects/lists
+        frontier = queue.PriorityQueue()
+        self.add_path(self, "start")
+        frontier.put((0,self))
+        explored = set()
+        frontCount = 1
+        # start looping through the breadth
+        while not frontier.empty():
+            pnum, node = frontier.get()     # pop from the frontier
+            explored.add(node)           # add the board to the explored list
+            if str(node) == str(solution):          # check if destination reached
+                for k, v in node._path.items():     # loop to print the route
+                    print("{}\t{}".format(v, k))
+                return "success:\npath cost: {}\nfrontier: {}\nexplored: {}".format(len(node._path)-1, frontCount, len(explored))
+            for m, n in node.successors().items():          # obtain all possible moves, m = "left", "right", etc. n = puuzzle board
+                if n and (n not in explored):               # and (not self.in_priority_queue(frontier, n)):
+                    n._path = copy.deepcopy(node._path)     # deep copy the traveled course
+                    n.add_path(n, m)                        # add new node to the route
+                    frontCount += 1
+                    frontier.put((len(node._path)-1, n))
+                else:
+                    self.update_priority_queue(frontier, (len(node._path)-1,n))     # this command checks that if n is in frontier, if frontier(n).cost>n.cost, and updates it
+        return "fail:\npath cost: {}\nfrontier: {}\nexplored: {}".format(len(node._path)-1, frontCount, len(explored))
 
-<<<<<<< Updated upstream
-        return 0
-=======
-    def manhanttan_func(self, current, solution):
-        if solution:
-            #if solution is valid
-            for x in range(3):
-                for y in range(3):
-                    current = self._get_tile(x,y)
-                    #get current tile
-                    #find tile in solution
-                    for x2 in range(3):
-                        for y2 in range (3):
-                            if current == solution._get_tile(x2, y2):
-                            #if you find the tile in the solution tile
-                                locSoluTile  = (x2, y2)
-                                #store the location of the tile
-                                
->>>>>>> Stashed changes
 
     def greedy_best_first_search(self, solution, i = 0):
 
@@ -68,16 +75,60 @@ class EightPuzzleBoard_Solver(puzz.EightPuzzleBoard):
 
         return 0
 
+    def in_queue(self, frontier, n):
+        # ******** this section of code checks "n in frontier" ********
+        boo = False
+        temp = queue.Queue()
+        while not frontier.empty():
+            node = frontier.get()
+            if node is n:
+                boo = True
+            temp.put(node)
+        while not temp.empty():
+            frontier.put(temp.get())
+        # ********************
+        return boo
+
+    def in_priority_queue(self, frontier, n):
+        # ******** this section of code checks "n in frontier" ********
+        boo = False
+        temp = queue.Queue()
+        while not frontier.empty():
+            node = frontier.get()
+            if node[1] is n:
+                boo = True
+            temp.put(node)
+        while not temp.empty():
+            frontier.put(temp.get())
+        # ********************
+        return boo
+
+    def update_priority_queue(self, frontier, n):
+        # ******** this section of code checks "n in frontier" ********
+        boo = False
+        temp = queue.Queue()
+        while not frontier.empty():
+            node = frontier.get()
+            if node[1] is n[1] and node[0] > n[0]:
+                temp.put(n)
+            else:
+                temp.put(node)
+        while not temp.empty():
+            frontier.put(temp.get())
+        # ********************
+        return
 
 
 if __name__ == '__main__':
-    board = EightPuzzleBoard_Solver('346105278')
+    board = EightPuzzleBoard_Solver('034615278')
     solution = puzz.EightPuzzleBoard('046315278')
-    solution = board.success_down()
-    solution = board.success_left()
-    print(board.breadth_search(solution))
+    solution = board.success_up()
+    solution = solution.success_left()
+    solution = solution.success_left()
+    #solution = solution.success_down()
 
-   # solution = EightPuzzleBoard('12345678')
-   # board.breadth_search(solution)
-   # board.uniform_cost_search(solution)
+
+
+    print(board.uniform_cost_search(solution))
+    print("this is solution:{}".format(solution))
 
